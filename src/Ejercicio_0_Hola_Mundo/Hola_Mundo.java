ba692d123d2eae65d3a386955f3a5116f3a13cee
Isaac_Beltran44 <isaac.beltran.arevalo@gmail.com>
2026-09-22 17:52:29 +0200

    Segundo Commit, Ejercicios Hechos

--- src/Ejercicio_0_Hola_Mundo/Hola_Mundo.java
+++ src/Ejercicio_0_Hola_Mundo/Hola_Mundo.java
@@ -6,11 +6,11 @@ public class Hola_Mundo {
     public static void main(String[] args) {
 
         //EJ 1
-        System.out.printf("Hola ");
-        System.out.printf("Mundo!!!\n");
+        System.out.print("Hola ");
+        System.out.print("Mundo!!!\n");
 
         //EJ 2
-        System.out.printf("Ho\n la\n Mun\n do!!!");
+        System.out.print("Ho\n la\n Mun\n do!!!");
 
     }
 }
